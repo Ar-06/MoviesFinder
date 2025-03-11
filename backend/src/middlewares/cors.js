a6450c8c426870bf47ca8666c0cfd138a2203306
Ar-06 <fabiancrack2@gmail.com>
2025-03-11 18:45:26 -0500

    Update cors.js

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -3,6 +3,7 @@ import cors from "cors";
 const ACCEPTED_ORIGIN = [
   "http://localhost:5173",
   "https://movies-finder-f61zf3df7-ar-06s-projects.vercel.app",
+  "https://moviesfinder-aipi.onrender.com/",
 ];
 
 export const corsMiddleware = ({ acceptedOrigin = ACCEPTED_ORIGIN } = {}) =>
