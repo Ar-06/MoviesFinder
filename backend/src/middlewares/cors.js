7657442e7e54d9c87575ffab56f459698109416d
Ar-06 <fabiancrack2@gmail.com>
2025-03-11 18:06:16 -0500

    Update cors.js

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -1,19 +1,20 @@
-import cors from 'cors'
+import cors from "cors";
 
 const ACCEPTED_ORIGIN = [
-    'http://localhost:5173'
-]
+  "http://localhost:5173",
+  "https://movies-finder-qgpg4kon0-ar-06s-projects.vercel.app/",
+];
 
-
-export const corsMiddleware = ({ acceptedOrigin = ACCEPTED_ORIGIN } = {}) => cors({
+export const corsMiddleware = ({ acceptedOrigin = ACCEPTED_ORIGIN } = {}) =>
+  cors({
     origin: (origin, callback) => {
-        if (acceptedOrigin.includes(origin)) {
-            return callback(null, true)
-        }
-        if (!origin) {
-            return callback(null, true)
-        }
-        return callback(new Error('Not allowed by CORS'))
+      if (acceptedOrigin.includes(origin)) {
+        return callback(null, true);
+      }
+      if (!origin) {
+        return callback(null, true);
+      }
+      return callback(new Error("Not allowed by CORS"));
     },
-    credentials: true
-})
+    credentials: true,
+  });
