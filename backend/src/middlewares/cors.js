43822600ee51dcdbd7b6521d7c24a3cc8b169bb0
Ar-06 <fabiancrack2@gmail.com>
2025-03-11 19:30:26 -0500

    Update cors.js file

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -2,21 +2,16 @@ import cors from "cors";
 
 const ACCEPTED_ORIGIN = [
   "http://localhost:5173",
-  "https://movies-finder-f61zf3df7-ar-06s-projects.vercel.app",
-  "https://moviesfinder-aipi.onrender.com",
-  "https://carefree-celebration-production.up.railway.app",
+  "https://movies-finder-five.vercel.app",
+  "https://moviesfinder-production.up.railway.app",
 ];
 
 export const corsMiddleware = ({ acceptedOrigin = ACCEPTED_ORIGIN } = {}) =>
   cors({
     origin: (origin, callback) => {
-      console.log("🔍 Origin recibido:", origin); // Agregar log para depuración
-
       if (!origin || acceptedOrigin.includes(origin)) {
         return callback(null, true);
       }
-
-      console.error("🚫 Bloqueado por CORS:", origin);
       return callback(new Error("Not allowed by CORS"));
     },
     credentials: true,
